25334f985a7d683ba62d133be43ec5484b0bd240
Hritvik Gupta <hritvik7654@gmail.com>
2025-02-12 13:04:07 -0500

    5feb push

--- src/config/constants.js
+++ src/config/constants.js
@@ -19,23 +19,23 @@
 
 
 
-import path from 'path';
+// import path from 'path';
 
-export const BASE_PREFIX = "MVP_R4.1000G_";
-export const TOP_HITS_PATH = "/Users/hritvik/Downloads/tabix_file/top_hits/";
-export const GWAS_FILES_PATH = "/nfs/platlas_stor/tabix/";
-export const MANIFEST_PATH = "/home/ac.guptahr/platlas-backend/DATABASE/metadata_platlas.csv";
-export const ARGONNE_ACCESS_TOKEN = 'Ag2j86JY3Gg6reYe9lE87zlKpOM00XJWDDgxYKMVk7zoE6OpN7CkCoM0vB7V6regrWyyEv5xjX3EglfwDV5N6CQMmJE'
+// export const BASE_PREFIX = "MVP_R4.1000G_";
+// export const TOP_HITS_PATH = "/Users/hritvik/Downloads/tabix_file/top_hits/";
+// export const GWAS_FILES_PATH = "/nfs/platlas_stor/tabix/";
+// export const MANIFEST_PATH = "/home/ac.guptahr/platlas-backend/DATABASE/metadata_platlas.csv";
+// export const ARGONNE_ACCESS_TOKEN = 'Ag2j86JY3Gg6reYe9lE87zlKpOM00XJWDDgxYKMVk7zoE6OpN7CkCoM0vB7V6regrWyyEv5xjX3EglfwDV5N6CQMmJE'
 
-// export const SQLITE_DB_PATH = "/Users/hritvik/genomics-backend/phewas_database.db";``
-export const GWAMA_DB = "/nfs/platlas_stor/db/genomics-backend/phewas_gwama.db";``
-export const MRMEGA_DB = "/nfs/platlas_stor/db/genomics-backend/phewas_mrmega.db";``
-export const LEAD_MRMEGA_PATH = "/home/ac.guptahr/platlas-backend/DATABASE/Combined_SQLite.csv";
-// Add to constants.js
-export const TABIX_BASE_PATH = 'https://g-fce312.fd635.8443.data.globus.org/tabix';
+// // export const SQLITE_DB_PATH = "/Users/hritvik/genomics-backend/phewas_database.db";``
+// export const GWAMA_DB = "/nfs/platlas_stor/db/genomics-backend/phewas_gwama.db";``
+// export const MRMEGA_DB = "/nfs/platlas_stor/db/genomics-backend/phewas_mrmega.db";``
+// export const LEAD_MRMEGA_PATH = "/home/ac.guptahr/platlas-backend/DATABASE/Combined_SQLite.csv";
+// // Add to constants.js
+// export const TABIX_BASE_PATH = 'https://g-fce312.fd635.8443.data.globus.org/tabix';
 
-export const COMBINED_SNP_INFO = '/home/ac.guptahr/platlas-backend/DATABASE/combined_SNPs.csv'
-export const SNP_MAPPING = '/home/ac.guptahr/platlas-backend/DATABASE/gwPheWAS_All.annotation.txt.gz'
+// export const COMBINED_SNP_INFO = '/home/ac.guptahr/platlas-backend/DATABASE/combined_SNPs.csv'
+// export const SNP_MAPPING = '/home/ac.guptahr/platlas-backend/DATABASE/gwPheWAS_All.annotation.txt.gz'
 
 
 
